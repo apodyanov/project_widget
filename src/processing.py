71210fbeda68657b9d5398f442c4905ecdf6bd83
Andrey Podianov <podyanov82@gmail.com>
2025-09-14 23:21:05 +0500

    add sort function

--- src/processing.py
+++ src/processing.py
@@ -18,17 +18,40 @@ from typing import List, Dict, Any, Optional
 
 
 def filter_by_state(data: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
+
     # Проверка на некорректность типа введенных данных
     if not isinstance(data, list):
-        raise TypeError("Data must be a list of dictionaries")
+        raise TypeError("Тип вводимых данных должен быть 'списком' или 'словарем'")
 
     if not isinstance(state, str):
-        raise TypeError("State must be a string")
+        raise TypeError("Тип аргумента 'State' должен быть строкой")
 
     return [item for item in data if item.get("state") == state]
 
-result = filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}
-            ])
-print(result)
+
+"""Функция sort_by_date, которая принимает список словарей и необязательный
+параметр, задающий порядок сортировки (по умолчанию — убывание).
+Функция должна возвращать новый список, отсортированный по дате (date)."""
+
+def sort_by_date(
+    data: List[Dict[str, Any]],
+    descending: bool = True
+) -> List[Dict[str, Any]]:
+    # Проверка на некорректность типа введенных данных
+    if not isinstance(data, list):
+        raise TypeError("Тип вводимых данных должен быть 'списком' или 'словарем'")
+
+    if not data:
+        return []
+
+    # Проверяем что все словари имеют ключ date
+    for item in data:
+        if "date" not in item:
+            raise KeyError("Все словари должны содержать ключ 'date'")
+
+        # Сортируем по дате (ISO формат корректно сортируется как строка)
+    return sorted(
+        data,
+        key=lambda x: x["date"],
+        reverse=descending
+    )
