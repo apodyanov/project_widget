397bc19affc067eb5e9c414cc8702c4052ce43e4
Andrey Podianov <podyanov82@gmail.com>
2025-10-20 21:03:26 +0500

    логирование файла masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,11 +1,10 @@
 import logging
-from venv import logger
 
 
-logger = logging.getLogger('masks')
+logger = logging.getLogger("masks")
 logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler('logs/masks.log', encoding='utf-8')
-file_formatter = logging.Formatter('%(asctime)s : %(name)s : %(levelname)s : %(message)s')
+file_handler = logging.FileHandler("logs/masks.log", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s : %(name)s : %(levelname)s : %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
@@ -17,16 +16,16 @@ def get_mask_card_number(card_number: str) -> str:
     """Функция маскировки номера банковской карты показывает
     первые шесть и последние четыре цифры."""
     try:
-        logger.info(f'Проверка корректного ввода номера карты')
+        logger.info("Проверка корректного ввода номера карты")
         if not card_number.isdigit():
-            logger.error(f'Не корректный ввод номера карты')
+            logger.error("Не корректный ввод номера карты")
             raise ValueError("Номер карты должен содержать только цифры")
 
         if len(card_number) != 16:
-            logger.error(f'Не корректный ввод номера карты')
+            logger.error("Не корректный ввод номера карты")
             raise ValueError("Номер карты должен быть только из 16 цифр")
 
-        logger.info(f'Формирование маски номера карты')
+        logger.info("Формирование маски номера карты")
         # Разбиваем на группы по 4 цифры
         part1 = card_number[:4]
         part2 = card_number[4:6]  # Первые 2 цифры второй группы
@@ -35,9 +34,8 @@ def get_mask_card_number(card_number: str) -> str:
 
         return f"{part1} {part2}** {part3} {part4}"
     except ValueError as ex:
-        logger.error(f'Произошла ошибка: {ex}')
-        return f'Произошла ошибка'
-
+        logger.error("Произошла ошибка: %s", ex)
+        return "Произошла ошибка"
 
 
 def get_mask_account(account_number: str) -> str:
@@ -45,19 +43,19 @@ def get_mask_account(account_number: str) -> str:
     только последние четыре цифры номера счета."""
 
     try:
-        logger.info(f'Проверка корректного ввода счета')
+        logger.info("Проверка корректного ввода счета")
         if not account_number.isdigit():
-            logger.error(f'Не корректный ввод номера счета')
+            logger.error("Не корректный ввод номера счета")
             raise ValueError("Номер счета должен содержать только цифры")
 
-        logger.info(f'Проверка на минимально допустимое количество цифр счета')
+        logger.info("Проверка на минимально допустимое количество цифр счета")
         if len(account_number) < 4:
-            logger.error(f'Номер счета должен содержать минимум 4 цифры')
+            logger.error("Номер счета должен содержать минимум 4 цифры")
             raise ValueError("Номер счета должен содержать минимум 4 цифры")
 
         # Показываем только последние 4 цифры
-        logger.info(f'Формирование маски номера счета')
+        logger.info("Формирование маски номера счета")
         return f"**{account_number[-4:]}"
     except ValueError as ex:
-        logger.error(f'Произошла ошибка: {ex}')
-        return f'Произошла ошибка'
+        logger.error("Произошла ошибка: %s", ex)
+        return "Произошла ошибка"
