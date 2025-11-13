221aa3d9edd516cff447f85e95a5bdc4a0db7301
Andrey Podianov <podyanov82@gmail.com>
2025-11-13 12:48:37 +0500

    readme

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 """Главный модуль запуска программы."""
-from config import TRANSACTIONS_CSV_FILE_PATH, TRANSACTIONS_EXCEL_FILE_PATH
 
-from src.read_csv_excel import read_transactions_csv, read_transactions_excel
+
+
+
+
+
 
 
 if __name__ == "__main__":
@@ -16,17 +19,6 @@ if __name__ == "__main__":
 
 
 
-print('-'*50)
-print('Домашнее задание 13.1 Библиотеки csv и pandas')
-print()
-print('='*50)
-
-print("\nФункция для чтения и вывода CSV файла")
-print("-" * 30)
-read_transactions_csv(TRANSACTIONS_CSV_FILE_PATH)
-print("\nФункция для чтения и вывода EXCEL файла")
-print("-" * 30)
-read_transactions_excel(TRANSACTIONS_EXCEL_FILE_PATH)
 
 
 
--- src/read_csv_excel.py
+++ src/read_csv_excel.py
@@ -55,3 +55,16 @@ def read_transactions_excel(file_path: str | None = None) -> list[dict[Hashable,
     except Exception as e:
         print(f"Ошибка: {e}")
         return []
+
+
+print('-'*50)
+print('Домашнее задание 13.1 Библиотеки csv и pandas')
+print()
+print('='*50)
+
+print("\nФункция для чтения и вывода CSV файла")
+print("-" * 30)
+read_transactions_csv(TRANSACTIONS_CSV_FILE_PATH)
+print("\nФункция для чтения и вывода EXCEL файла")
+print("-" * 30)
+read_transactions_excel(TRANSACTIONS_EXCEL_FILE_PATH)
